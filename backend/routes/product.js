12d17008b13daae275e4089c8a3fa9e3e47bb206
Priya <priya.shah174@gmail.com>
2021-09-29 11:51:30 +0200

    added the post route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
 	getProducts,
+	newProduct,
 } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
+router.route('/product/new').post(newProduct);
 
 module.exports = router;

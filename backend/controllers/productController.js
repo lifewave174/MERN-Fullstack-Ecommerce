403896b57ce8ba6b7774d5868639c19702581c41
dci <priya.shah174@gmail.com>
2021-09-29 15:26:47 +0200

    seeder set up and working

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,18 +1,18 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 //Create new product=> /api/v1/product/new
 
 exports.newProduct = async (req, res, next) => {
-	const product = await Product.create(req.body);
-	res.status(201).json({
-		success: true,
-		product,
-	});
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
 };
 
 exports.getProducts = (req, res, next) => {
-	res.status(200).json({
-		success: true,
-		message: 'This route will show all products in database',
-	});
+  res.status(200).json({
+    success: true,
+    message: "This route will show all products in database",
+  });
 };

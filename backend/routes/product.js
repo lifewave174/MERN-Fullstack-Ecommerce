de07776dfa9d19625a713e06400ae1839369f0e7
dci <priya.shah174@gmail.com>
2021-09-29 16:36:24 +0200

    created the update and delete controllers

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const {
-	getProducts,
-	newProduct,
+  getProducts,
+  newProduct,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
 } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
-router.route('/product/new').post(newProduct);
+router.route('/products/:id').get(getSingleProduct);
+
+router.route('/admin/product/new').post(newProduct);
+router.route('/admin/products/:id').put(updateProduct).delete(deleteProduct);
 
 module.exports = router;

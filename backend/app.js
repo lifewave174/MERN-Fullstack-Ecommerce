74f9e1e468a056bc0be3b494b8c18e232084bd6b
dci <priya.shah174@gmail.com>
2021-09-30 15:13:21 +0200

    created errorhandler for async errors and unhandled promise rejections

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+const errorMiddleware = require('./middleware/errors');
 
 app.use(express.json());
 
-const products = require("./routes/product");
+const products = require('./routes/product');
 
-app.use("/api/v1", products);
+app.use('/api/v1', products);
+
+//Middleware to handle errors
+app.use(errorMiddleware);
+
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,31 @@ const connectDatabase = require('./config/database');
 
 const dotenv = require('dotenv');
 
+//Handle Uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log('Shutting down server due to uncaught exceptions');
+  process.exit(1);
+});
+
 //Setting up config file
 dotenv.config({ path: 'backend/config/config.env' });
 
 //Connecting to Database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
-	console.log(
-		`server has started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
-	);
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `server has started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
+
+//Handle Unhandled Promise rejections
+
+process.on('unhandledRejection', (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log('Shutting down the server due to Unhandled Promise Rejection');
+  server.close(() => {
+    process.exit(1);
+  });
 });

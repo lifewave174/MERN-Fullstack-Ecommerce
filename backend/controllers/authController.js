cf3f636ad6c8b885e480739f8511459f9d8b583a
dci <priya.shah174@gmail.com>
2021-10-04 17:10:35 +0200

    stored cookie in the login and register user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 //Register a user => api/v1/register
 
@@ -18,10 +19,34 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  const token = user.getJwtToken();
+  sendToken(user, 200, res);
+});
 
-  res.status(201).json({
-    success: true,
-    token,
-  });
+//Login User => /api/v1/Login
+
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Checks if email and password are entered by userSchema
+
+  if (!email || !password) {
+    return next(new ErrorHandler('Please enter email and password', 400));
+  }
+
+  //Finding user in database
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorHandler('Invalid Email or Password', 401));
+  }
+
+  //Checks if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler('Invalid Email or Password', 401));
+  }
+
+  sendToken(user, 200, res);
 });
